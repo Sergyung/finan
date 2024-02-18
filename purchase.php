8303a01b5793c50aa2f29f690a4f129008c0e481
Sergej A <sergbest@mail.ru>
2024-02-19 00:50:59 +0300

    working with a file purchase.php

--- purchase.php
+++ purchase.php
@@ -22,11 +22,9 @@
 					<?php
 	        		$query = "SELECT * FROM papers"; 
 					$result = mysqli_query($link, $query);
-					 
 					while($row = $result->fetch_assoc())
 					{
 					    echo '<option value ='.$row['id_paper'].'>'.$row['name_paper'].'</option>';
-					    //echo '<input type="hidden" name="count" value="'.$row['lot_paper'].'">';
 					}
 				?>
 				</select></td>
@@ -71,20 +69,22 @@ if(isset($_POST['submit'])){
 		die( "Неуказано название бумаги.");
 	}
 
-	/* 
+	// Определем количество бумаг в одном лоте 
 
-	Пробую вывести нужное количество бумаг 
+	$query1 = "SELECT * FROM papers WHERE id_paper = '$name'"; // WHERE id = 3
+	$result = mysqli_query($link, $query1);
+	$res = mysqli_fetch_assoc($result);
+	
+	$count = $res['lot_paper'];
+		
+	$transactPurch = 1; // номер транзакции 1 - покупка
+	$transactComm = 2; // номер транзакции 2 - комиссия брокера
 
-	$query1 = "SELECT * FROM papers WHERE id = 3"; 
-	$result1 = mysqli_query($link, $query1);
-	$row1 = mysqli_fetch_assoc($result1);
-	echo($row1['lot_paper']);
-
-	*/
-
-	$transact = 1;
-
-	$date = $_POST['date'];
+	if(isset($_POST["date"]) && !empty($_POST["date"])){
+		$date = $_POST['date'];
+	} else {
+		die( "Неверно указана дата покупки.");
+	}
 
 	if(is_numeric($_POST['lot']) && $_POST['lot'] > 0){
 		$lot = $_POST['lot'];
@@ -98,11 +98,23 @@ if(isset($_POST['submit'])){
 		die( "Неверно указана цена купленных бумаг.");
 	}
 
-	$sql_purch = "INSERT INTO actions (id_paper, id_transac, lot, date_action, price_paper) VALUES ('$name', '$transact',  '$lot', '$date', '$price')";
+	if(is_numeric($_POST['award']) && $_POST['award'] > 0){
+		$award = $_POST['award'];
+	} else {
+		die( "Неверно указана цена купленных бумаг.");
+	}
+
+	$sum = $count * $lot * $price;
+
+	$sql_purch = "INSERT INTO actions (id_paper, id_transac, lot, date_action, price_paper, sum_action) VALUES ('$name', '$transactPurch',  '$lot', '$date', '$price', $sum)";
 
 	$result_purch = mysqli_query($link, $sql_purch) or die( mysqli_error($link) );
 
-	if (isset($result_purch)) {
+	$sql_comm = "INSERT INTO actions (id_paper, id_transac, date_action, sum_action) VALUES ('$name', '$transactComm',  '$date', $award)";
+
+	$result_comm = mysqli_query($link, $sql_comm) or die( mysqli_error($link) );
+
+	if (isset($result_purch) && isset($result_comm)) {
 		echo '<h4>Запрос на добавление в базу данных прошел.</h4>';
 	}
 }
