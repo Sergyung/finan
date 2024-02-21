00b8228fb7d1913f536b82c07c5e04de0b4039cc
Sergyung <sergbest@mail.ru>
2024-02-21 16:16:12 +0300

    movement.php revision of the table output

--- movement.php
+++ movement.php
@@ -14,27 +14,30 @@
 <body>
 	<h2>Покупка</h2>
 	<form method="post" action=""> <!--addPurchase.php-->
-		<table>
-			
+		<table border="1">
+			<tr>
+				<td colspan="5"><a href="index.php">Вернуться на главную</a></td>
+				
+			</tr>
+
 			<?php
 	        	$query = "SELECT * FROM actions LEFT JOIN papers ON papers.id_paper = actions.id_paper INNER JOIN transactions ON transactions.id = actions.id_transac ORDER BY actions.id"; 
 				$result = mysqli_query($link, $query);
 				while($row = $result->fetch_assoc())
 				{
 					echo '<tr>
-					<td>'.$row['name_paper'].'</td>
-					<td>'.$row['code'].'</td>
-					<td>'.$row['date_action'].'</td>
-					<td>'.$row['sum_action'].'</td>
+					<td width="100">'.$row['date_action'].'</td>
+					<td width="170">'.$row['code'].'</td>
+					<td width="300">'.$row['name_paper'].'</td>
+					<td width="100">'.$row['sum_action'].'</td>
+					<td width="100">'.$row['balance'].'</td>
 					</tr>';
 				    //echo '<option value ='.$row['id_paper'].'>'..'</option>';
 				}
 			?>
-			
-					
-			
+						
 			<tr>
-				<td colspan="2"><a href="index.php">Вернуться на главную</a></td>
+				<td colspan="5"><a href="index.php">Вернуться на главную</a></td>
 				
 			</tr>
 		</table>
